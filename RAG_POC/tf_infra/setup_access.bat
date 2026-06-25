6b0aeb3c0f7f898c26218b631c3278155b592518
Aleksandar Todorov <linked2u@gmail.com>
2026-06-25 13:58:40 +0300

    Created a folder for RAG PoC which will hold the IaC configuration for AMI and PoC machine

---
+++ RAG_POC/tf_infra/setup_access.bat
@@ -0,0 +1,5 @@
+REM This batch is used to setup the AWS API key 
+REM which will be used by terraform in order to deploy the dev machine
+REM intended to be used as PoC
+
+set /P AWS_ACCESS_KEY="Enter your AWS ACCESS KEY: "
