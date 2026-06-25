cd3b0854f35dc329423409ee7b772a548f85c7e4
Aleksandar Todorov <linked2u@gmail.com>
2026-06-25 16:31:41 +0300

    addded a script that will install the pckgs considered needed for the RAG; adeed python requirements file for the modules needed

---
+++ RAG_POC/tf_infra/packer_config/scripts/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+REQUIRED_PACKAGES=(
+  git
+  vim
+  curl
+  wget
+  unzip
+  jq
+  tree
+  python3
+  python3-pip
+  python3-venv
+  postgresql
+  postgresql-contrib
+  build-essential
+  libpq-dev
+)
