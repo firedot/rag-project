abb2aecbfa6fd48fedc98560adb63171005bc126
Aleksandar Todorov <linked2u@gmail.com>
2026-06-25 16:54:29 +0300

    added venv creation; added pip modules installation

--- RAG_POC/tf_infra/packer_config/scripts/bootstrap.sh
+++ RAG_POC/tf_infra/packer_config/scripts/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 REQUIRED_PACKAGES=(
   git
@@ -16,3 +17,21 @@ REQUIRED_PACKAGES=(
   build-essential
   libpq-dev
 )
+
+sudo apt-get update
+
+
+for package in "${REQUIRED_PACKAGES[@]}"; do
+  if dpkg -s "$package" >/dev/null 2>&1; then
+    echo "[OK] $package is already installed"
+  else
+    echo "[INSTALL] $package"
+    sudo apt-get install -y "$package"
+  fi
+done
+
+mkdir ~/rag_poc
+cd ~/rag_poc
+python3 -m venv .venv
+source .venv/bin/activate 
+pip install -r /tmp/requirements.txt
